37d31eb5f960b2d58b5fcb3e0933543b82049aa9
NielsenFranco <franconielsen97@hotmail.com.ar>
2024-11-07 20:30:49 -0300

    #41-cambios al sql

--- sql/sevd.sql
+++ sql/sevd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-11-2024 a las 00:25:30
+-- Tiempo de generación: 08-11-2024 a las 00:29:12
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
